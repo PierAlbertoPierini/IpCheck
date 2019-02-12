7a20169eb1952d045b8e73965d15471426bac66a
Pier Alberto <pieralbertopierini@gmail.com>
2019-02-12 21:43:59 +0900

    Added comment and the program find the last entry from a csv file

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # IP Check
 
-I made this Golang program to have a self automated discoverable IP address to be able to connect to my PC from Internet.
+I made this Golang program to have a self automated discoverable IP address to be able to connect to my PC from Internet with out use other services.
--- main.go
+++ main.go
@@ -1,48 +1,83 @@
-# -*- coding: utf-8 -*-
+// -*- coding: utf-8 -*-
 
-#  Copyright 2017 Pier Alberto <pieralbertopierini@gmail.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#
-#
+//  Copyright 2019 Pier Alberto <pieralbertopierini@gmail.com>
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
 
-# Built on Linux
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+//  MA 02110-1301, USA.
+//
+//
+
+// Built on Linux
 
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"io"
+	"log"
 	"os"
 
 	"github.com/rdegges/go-ipify"
 )
 
 func main() {
-	// Check if the iplist.data file exist, if not the file is created
-	data, err := os.OpenFile("iplist.data", os.O_RDWR|os.O_CREATE, 0664)
+
+	//Definitions of variables
+	var oldIP string
+
+	// Check presence of data file iplist.data if is not present create it
+	// Open and read iplist.csv put in memory the last IP
+	f, err := os.OpenFile("iplist.csv", os.O_RDWR|os.O_CREATE, 0664)
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Println(err)
 	}
 
-	fmt.Println(string(data))
-	ip, err := ipify.GetIp()
+	defer f.Close()
+
+	r := csv.NewReader(f)
+
+	for {
+		record, err := r.Read()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		//fmt.Println(record[0])
+		oldIP = record[0]
+	}
+	fmt.Println("The old IP was: ", oldIP)
+	//Check current IP
+	newIP, err := ipify.GetIp()
 	if err != nil {
 		fmt.Println("Couldn't get my IP address:", err)
 	} else {
-		fmt.Println("My IP address is:", ip)
+		fmt.Println("My IP address is:", newIP)
 	}
+	// Check if old and new IP are equal
+	if oldIP == newIP {
+		fmt.Println("The IPs are equal")
+	} else {
+		fmt.Println("The IPs are different")
+		//send an email and affix the new IP on the iplist.data if there are a new IP
+	}
+
+	//wait 5 minutes and restart the loop
 }
